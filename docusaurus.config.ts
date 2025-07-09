47c6f7d0ca9eeafc74fef7fb715e37858e577811
SageInfinity <akp.fb343@gmail.com>
2025-07-09 13:00:33 +0530

    local

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -16,7 +16,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://sageinfinity.github.io/LosslessScalingGuides/',
+  url: 'https://sageinfinity.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
@@ -25,7 +25,7 @@ const config: Config = {
   // If you aren't using GitHub pages, you don't need these.
   //organizationName: 'facebook', // Usually your GitHub org/user name.
   projectName: 'LosslessScalingGuides',
-
+  organizationName: 'SageInfinity',
   onBrokenLinks: 'warn',
   onBrokenMarkdownLinks: 'warn',
 
